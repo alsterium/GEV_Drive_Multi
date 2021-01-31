5f8439b568348bac60d48b0e7863a9e707b60be5
alsterium <new033@gmail.com>
2021-01-31 15:04:13 +0900

    画像を取得するタイミングでトリガーを発信し，シャッタータイミングを同期させる

--- GEV_Wrapper.cpp
+++ GEV_Wrapper.cpp
@@ -42,6 +42,134 @@ std::tuple<cv::Mat, cv::Mat> initMap(const std::string intrinsicsFile)
 	return {map1, map2};
 }
 
+// この関数は、トリガを使用するようにカメラを設定します。
+//まず、トリガモードをオフに設定し、トリガソースを選択します。
+//トリガソースを選択すると、トリガモードが有効になり、選択した
+//トリガの実行時にカメラは1枚の画像のみを撮影します。
+int ConfigureTrigger(INodeMap& nodeMap)
+{
+	int result = 0;
+
+	cout << endl << endl << "*** CONFIGURING TRIGGER ***" << endl << endl;
+
+	cout << "Note that if the application / user software triggers faster than frame time, the trigger may be dropped "
+		"/ skipped by the camera."
+		<< endl
+		<< "If several frames are needed per trigger, a more reliable alternative for such case, is to use the "
+		"multi-frame mode."
+		<< endl
+		<< endl;
+
+		cout << "Software trigger chosen..." << endl;
+
+	try
+	{
+		//
+		// Ensure trigger mode off
+		//
+		// *** NOTES ***
+		// The trigger must be disabled in order to configure whether the source
+		// is software or hardware.
+		//
+		CEnumerationPtr ptrTriggerMode = nodeMap.GetNode("TriggerMode");
+		if (!IsAvailable(ptrTriggerMode) || !IsReadable(ptrTriggerMode))
+		{
+			cout << "Unable to disable trigger mode (node retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		CEnumEntryPtr ptrTriggerModeOff = ptrTriggerMode->GetEntryByName("Off");
+		if (!IsAvailable(ptrTriggerModeOff) || !IsReadable(ptrTriggerModeOff))
+		{
+			cout << "Unable to disable trigger mode (enum entry retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		ptrTriggerMode->SetIntValue(ptrTriggerModeOff->GetValue());
+
+		cout << "Trigger mode disabled..." << endl;
+
+		//
+		// Set TriggerSelector to FrameStart
+		//
+		// *** NOTES ***
+		// For this example, the trigger selector should be set to frame start.
+		// This is the default for most cameras.
+		//
+		CEnumerationPtr ptrTriggerSelector = nodeMap.GetNode("TriggerSelector");
+		if (!IsAvailable(ptrTriggerSelector) || !IsWritable(ptrTriggerSelector))
+		{
+			cout << "Unable to set trigger selector (node retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		CEnumEntryPtr ptrTriggerSelectorFrameStart = ptrTriggerSelector->GetEntryByName("FrameStart");
+		if (!IsAvailable(ptrTriggerSelectorFrameStart) || !IsReadable(ptrTriggerSelectorFrameStart))
+		{
+			cout << "Unable to set trigger selector (enum entry retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		ptrTriggerSelector->SetIntValue(ptrTriggerSelectorFrameStart->GetValue());
+
+		cout << "Trigger selector set to frame start..." << endl;
+
+		//
+		// Select trigger source
+		//
+		// *** NOTES ***
+		// The trigger source must be set to hardware or software while trigger
+		// mode is off.
+		//
+		CEnumerationPtr ptrTriggerSource = nodeMap.GetNode("TriggerSource");
+		if (!IsAvailable(ptrTriggerSource) || !IsWritable(ptrTriggerSource))
+		{
+			cout << "Unable to set trigger mode (node retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		// Set trigger mode to software
+		CEnumEntryPtr ptrTriggerSourceSoftware = ptrTriggerSource->GetEntryByName("Software");
+		if (!IsAvailable(ptrTriggerSourceSoftware) || !IsReadable(ptrTriggerSourceSoftware))
+		{
+			cout << "Unable to set trigger mode (enum entry retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		ptrTriggerSource->SetIntValue(ptrTriggerSourceSoftware->GetValue());
+
+		cout << "Trigger source set to software..." << endl;
+
+		//
+		// Turn trigger mode on
+		//
+		// *** LATER ***
+		// Once the appropriate trigger source has been set, turn trigger mode
+		// on in order to retrieve images using the trigger.
+		//
+
+		CEnumEntryPtr ptrTriggerModeOn = ptrTriggerMode->GetEntryByName("On");
+		if (!IsAvailable(ptrTriggerModeOn) || !IsReadable(ptrTriggerModeOn))
+		{
+			cout << "Unable to enable trigger mode (enum entry retrieval). Aborting..." << endl;
+			return -1;
+		}
+
+		ptrTriggerMode->SetIntValue(ptrTriggerModeOn->GetValue());
+
+		// NOTE: Blackfly and Flea3 GEV cameras need 1 second delay after trigger mode is turned on
+
+		cout << "Trigger mode turned back on..." << endl << endl;
+	}
+	catch (Spinnaker::Exception& e)
+	{
+		cout << "Error: " << e.what() << endl;
+		result = -1;
+	}
+
+	return result;
+}
+
 int ConfigureCustomImageSetting(Spinnaker::GenApi::INodeMap& nodeMap) {
 	int result = 0;
 	cout << endl << endl << "*** CONFIGURING CUSTOM IMAGE SETTINGS ***" << endl << endl;
@@ -157,6 +285,11 @@ int InitCameras(SystemPtr& system, CameraList &camList, const string cameraID[])
 			pCam->Init();
 			INodeMap& nodeMap = pCam->GetNodeMap();
 
+			// トリガーモードを設定する
+			err = ConfigureTrigger(nodeMap);
+			if (err < 0)
+				result = -1;
+
 			// カメラの画像設定を行う
 			err = ConfigureCustomImageSetting(nodeMap);
 			if (err < 0)
@@ -168,6 +301,48 @@ int InitCameras(SystemPtr& system, CameraList &camList, const string cameraID[])
 	catch (Spinnaker::Exception& e) {
 		cout << "Error: " << e.what() << endl;
 	}
+	return result;
+}
+
+// This function returns the camera to a normal state by turning off trigger
+// mode.
+int ResetTrigger(INodeMap& nodeMap)
+{
+	int result = 0;
+
+	try
+	{
+		//
+		// Turn trigger mode back off
+		//
+		// *** NOTES ***
+		// Once all images have been captured, turn trigger mode back off to
+		// restore the camera to a clean state.
+		//
+		CEnumerationPtr ptrTriggerMode = nodeMap.GetNode("TriggerMode");
+		if (!IsAvailable(ptrTriggerMode) || !IsReadable(ptrTriggerMode))
+		{
+			cout << "Unable to disable trigger mode (node retrieval). Non-fatal error..." << endl;
+			return -1;
+		}
+
+		CEnumEntryPtr ptrTriggerModeOff = ptrTriggerMode->GetEntryByName("Off");
+		if (!IsAvailable(ptrTriggerModeOff) || !IsReadable(ptrTriggerModeOff))
+		{
+			cout << "Unable to disable trigger mode (enum entry retrieval). Non-fatal error..." << endl;
+			return -1;
+		}
+
+		ptrTriggerMode->SetIntValue(ptrTriggerModeOff->GetValue());
+
+		cout << "Trigger mode disabled..." << endl << endl;
+	}
+	catch (Spinnaker::Exception& e)
+	{
+		cout << "Error: " << e.what() << endl;
+		result = -1;
+	}
+
 	return result;
 }
 
@@ -213,6 +388,27 @@ DWORD WINAPI AcquireImage(LPVOID lpParam) {
 	}
 }
 
+DWORD WINAPI TransmitTrigger(LPVOID lpParam) {
+	PCameraParam& param = *((PCameraParam*)lpParam);
+	try
+	{
+		INodeMap& nodeMap = param.pCam->GetNodeMap();
+		// ソフトウェアトリガーを発信
+		CCommandPtr ptrSoftwareTriggerCommand = nodeMap.GetNode("TriggerSoftware");
+		if (!IsAvailable(ptrSoftwareTriggerCommand) || !IsWritable(ptrSoftwareTriggerCommand)) {
+			cout << "Unable to execute trigger. Abortiong..." << endl;
+			return 99;
+		}
+		ptrSoftwareTriggerCommand->Execute();
+		return 1;
+	}
+	catch (const std::exception& e)
+	{
+		cout << "Error " << e.what() << endl;
+		return 0;
+	}
+}
+
 vector<cv::Mat> AquireMultiCamImagesMT(
 	CameraList &camList, 
 	const string cameraID[], 
@@ -227,6 +423,7 @@ vector<cv::Mat> AquireMultiCamImagesMT(
 		camListSize = camList.GetSize();
 		CameraPtr* pCamList = new CameraPtr[camListSize];
 		HANDLE* grabTheads = new HANDLE[camListSize];
+		HANDLE* transmitThreads = new HANDLE[camListSize];
 		PCameraParam* pParam = new PCameraParam[camListSize];
 
 		for (int i = 0; i < camListSize; i++) {
@@ -235,16 +432,42 @@ vector<cv::Mat> AquireMultiCamImagesMT(
 			pParam[i].pCam = pCamList[i];
 			pParam[i].map1 = map1;
 			pParam[i].map2 = map2;
+			transmitThreads[i] = CreateThread(nullptr, 0, TransmitTrigger, &pParam[i], 0, nullptr);
 			grabTheads[i] = CreateThread(nullptr, 0, AcquireImage, &pParam[i], 0, nullptr);
 			assert(grabTheads[i] != nullptr);
 		}
 
+		// トリガーを発信する
+		WaitForMultipleObjects(
+			camListSize,
+			transmitThreads,
+			TRUE,
+			INFINITE
+		);
+		// カメラ画像を取得する
 		WaitForMultipleObjects(
 			camListSize, // 待つスレッドの数
 			grabTheads,  // 待つスレッドへのハンドル 
 			TRUE,        // スレッドをすべて待つ
 			INFINITE     // 無限に待つ
 		);
+
+		// 各カメラのスレッドの戻り値を確認する
+		for (int i = 0; i < camListSize; i++) {
+			DWORD exitcode;
+			BOOL rc = GetExitCodeThread(transmitThreads[i], &exitcode);
+			if (!rc) {
+				cout << "Handle error from GetExitCodeThread() returned for camera at index " << i << endl;
+				result = -1;
+			}
+			else if (!exitcode) {
+				cout << "Transmit thread for camera at index " << i
+					<< " exited with errors."
+					"Please check onscreen print outs for error details"
+					<< endl;
+				result = -1;
+			}
+		}
 
 		// 各カメラのスレッドの戻り値を確認する
 		for (int i = 0; i < camListSize; i++) {
@@ -262,6 +485,7 @@ vector<cv::Mat> AquireMultiCamImagesMT(
 				result = -1;
 			}
 		}
+
 
 		// カメラ画像を取りだす
 		for (int i = 0; i < camListSize; i++) {
@@ -351,6 +575,8 @@ void GEV_Drive::EndAcquisition()
 	{
 		for (int i = 0; i < camList.GetSize(); i++) {
 			pCam = camList.GetByIndex(i);
+			INodeMap& nodeMap = pCam->GetNodeMap();
+			ResetTrigger(nodeMap);
 			pCam->EndAcquisition();
 		}
 	}
